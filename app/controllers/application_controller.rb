060e735006e56ce903f5d70ff757df3facdf0e2a
Felix Hermann <17felix2000@gmail.com>
2021-01-22 10:48:56 +0100

    new push

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
-    before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
-  def configure_permitted_parameters
-    # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :full_name])
+def configure_permitted_parameters
+  # For additional fields in app/views/devise/registrations/new.html.erb
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :full_name, :house_number, :plz, :street_name, :city, :avatar_url, :photo])
 
-    # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
-  end
+  # For additional in app/views/devise/registrations/edit.html.erb
+  devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_18_182555) do
+ActiveRecord::Schema.define(version: 2021_01_22_093138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,15 +81,19 @@ ActiveRecord::Schema.define(version: 2021_01_18_182555) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "username"
-    t.string "full_name"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "username"
+    t.string "full_name"
+    t.string "avatar_url"
+    t.integer "winpoints"
+    t.integer "house_number"
+    t.string "street_name"
+    t.integer "plz"
+    t.string "city"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "avatar_url"
-    t.integer "points"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
